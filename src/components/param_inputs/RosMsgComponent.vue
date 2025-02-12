e9524adf40bdff5cd45ee369236f205e6146b437
Doomse <Hoefer@fzi.de>
2025-02-12 15:48:13 +0100

    Improved form display, supports collapsing components

--- src/components/param_inputs/RosMsgComponent.vue
+++ src/components/param_inputs/RosMsgComponent.vue
@@ -28,48 +28,148 @@
  *  POSSIBILITY OF SUCH DAMAGE.
 -->
 <script setup lang="ts">
+import { computed, ref } from 'vue';
+
 
 const props = defineProps<{
-    field_dict: Record<string, never>,
-    field_types: Record<string, never>,
-    field_key: string[]
+  field_dict: Record<string, never>,
+  field_types: Record<string, any>,
+  field_key: string[]
+  render_content: boolean
 }>()
+
+type FieldType = 'plain' | 'sequence' | 'nested'
+
+const plain_fields = computed<Record<string, any>[]>(
+  () => getFields('plain')
+)
+
+const nested_fields = computed<Record<string, any>[]>(
+  () => getFields('nested')
+)
+
+const sequence_fields = computed<Record<string, any>[]>(
+  () => getFields('sequence')
+)
+
+const render_children = ref<Record<string, boolean>>({})
+nested_fields.value.forEach(element => {
+  render_children.value[element.name] = props.render_content
+});
+sequence_fields.value.forEach(element => {
+  render_children.value[element.name] = props.render_content
+});
+
+function getFields(type: FieldType): Record<string, any>[]{
+  const fields: Record<string, any>[] = []
+  Object.keys(props.field_types).forEach(key => {
+    if (determineFieldType(key) === type) {
+      const field = props.field_types[key]
+      field.name = key
+      fields.push(field)
+    }
+  })
+  return fields
+}
+
+function determineFieldType(key: string): FieldType {
+  if (props.field_types[key].nested_type === null) {
+    return 'plain'
+  }
+  if (props.field_types[key].own_type.startsWith('sequence')) {
+    return 'sequence'
+  }
+  return 'nested'
+}
+
+function invertRenderChild(key: string) {
+  render_children.value[key] = 
+    !render_children.value[key]
+}
 
 </script>
 
 <template>
-  <div 
-    v-if="field_types['nested_type'] === null"
-    class="input-group mt-2"
-  >
-    <span class="input-group-text">
-      {{ props.field_key[props.field_key.length - 1] }}
-    </span>
-    <input 
-      type="text" 
-      class="form-control" 
-      :value="field_types['default_value']"
-    >
-    <span class="input-group-text text-muted">
-      {{ field_types['own_type'] }}
-    </span>
-  </div>
-  <div v-else class="list-group-item">
-    <div>
-      {{ props.field_key[props.field_key.length - 1] }}
-      <span class="text-muted">{{ props.field_types['own_type'] }}</span>
+  <template v-if="props.render_content">
+    <div v-if="plain_fields" class="d-table py-1 w-100">
+      <div 
+        v-for="field in plain_fields"
+        :key="field.name"
+        class="input-group d-table-row"
+      >
+        <span class="input-group-text d-table-cell">
+          {{ field.name }}
+        </span>
+        <input 
+          type="text" 
+          class="form-control d-table-cell w-100" 
+          :value="field.default_value"
+        >
+        <span class="input-group-text text-muted d-table-cell">
+          {{ field.own_type }}
+        </span>
+      </div>
     </div>
-    <div v-if="(field_types['own_type'] as string).startsWith('sequence')">
-        Sequence Handling
+    <div v-if="nested_fields" class="list-group">
+      <div
+        v-for="field in nested_fields"
+        :key="field.name"
+        class="list-group-item"
+      >
+        <div @click="invertRenderChild(field.name)" class="d-flex w-100 cursor-pointer">
+          {{ field.name }}
+          <span class="text-muted">{{ field.own_type }}</span>
+          <font-awesome-icon
+            :icon="'fa-solid ' + (render_children[field.name] ? 'fa-angle-up' : 'fa-angle-down')"
+            class="ms-auto"
+            aria-hidden="true"
+          />
+        </div>
+        <RosMsgComponent 
+          :field_dict="props.field_dict"
+          :field_types="field.nested_type"
+          :field_key="props.field_key.concat(field.name)"
+          :render_content="render_children[field.name]"
+        />
+      </div>
     </div>
-    <div v-else class="list-group">
-      <RosMsgComponent 
-        v-for="key in Object.keys(field_types['nested_type'])"
-        :key="key"
-        :field_dict="props.field_dict"
-        :field_types="field_types['nested_type'][key]"
-        :field_key="props.field_key.concat(key)"
-      />
+    <div v-if="sequence_fields" class="list-group">
+      <div 
+        v-for="field in sequence_fields"
+        :key="field.name"
+        class="list-group-item"
+      >
+        <div @click="invertRenderChild(field.name)">
+          {{ field.name }}
+          <span class="text-muted">{{ field.own_type }}</span>
+          <font-awesome-icon
+            :icon="'fa-solid ' + (render_children[field.name] ? 'fa-angle-up' : 'fa-angle-down')"
+            class="ms-auto"
+            aria-hidden="true"
+          />
+        </div>
+        <div v-if="render_children[field.name]">
+          Sequence Handling
+        </div>
+      </div>
     </div>
-  </div>
+  </template>
 </template>
+
+<style lang="scss" scoped>
+
+.d-table .input-group:nth-last-child(n+2) * {
+  border-bottom-left-radius: 0;
+  border-bottom-right-radius: 0;
+}
+
+.d-table .input-group:nth-child(n+2) * {
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+.d-table .input-group *:last-child {
+  border-left: 0;
+}
+
+</style>
